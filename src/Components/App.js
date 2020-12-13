ee846ff6af82a2752b0982a08823c8820e9f041c
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-12-13 14:59:08 -0800

    adds directory to app

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import Header from "./Header";
 import TapRoomControl from "./TapRoomControl";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Signin from "./Signin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
+    // <React.Fragment>
+    //   <Header />
+    //   <TapRoomControl />
+    // </React.Fragment>
+    <Router>
       <Header />
-      <TapRoomControl />
-    </React.Fragment>
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+          <TapRoomControl />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
